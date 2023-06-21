a16d0daeef95425f5c72ad745fe4b6a037cbfcb7
SkyPro Backend Faculty <faculty.backend@skyeng.ru>
2023-06-21 18:52:28 +0200

    Changed security model to reflect the requirements

--- src/main/java/com/skypro/simplebanking/configuration/SecurityConfiguration.java
+++ src/main/java/com/skypro/simplebanking/configuration/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.skypro.simplebanking.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -13,41 +14,43 @@ import org.springframework.security.web.authentication.AnonymousAuthenticationFi
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
-  }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-  @Bean
-  public SecurityFilterChain httpSecurity(
-      HttpSecurity httpSecurity, AdminSecurityFilter adminSecurityFilter) throws Exception {
-    return httpSecurity
-        .csrf()
-        .disable()
-        .logout()
-        .disable()
-        .formLogin()
-        .disable()
-        .requestCache()
-        .disable()
-        .cors()
-        .and()
-        .httpBasic()
-        .and()
-        .sessionManagement()
-        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        .and()
-        .authorizeHttpRequests()
-        .antMatchers("/user/**")
-        .hasRole("ADMIN")
-        .antMatchers("/account/**")
-        .hasRole("USER")
-        .antMatchers("/transfer/**")
-        .hasRole("USER")
-        .anyRequest()
-        .authenticated()
-        .and()
-        .addFilterBefore(adminSecurityFilter, AnonymousAuthenticationFilter.class)
-        .build();
-  }
+    @Bean
+    public SecurityFilterChain httpSecurity(
+            HttpSecurity httpSecurity, AdminSecurityFilter adminSecurityFilter) throws Exception {
+        return httpSecurity
+                .csrf()
+                .disable()
+                .logout()
+                .disable()
+                .formLogin()
+                .disable()
+                .requestCache()
+                .disable()
+                .cors()
+                .and()
+                .httpBasic()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeHttpRequests()
+                .antMatchers(HttpMethod.POST, "/user/")
+                .hasRole("ADMIN")
+                .antMatchers("/user/**")
+                .hasRole("USER")
+                .antMatchers("/account/**")
+                .hasRole("USER")
+                .antMatchers("/transfer/**")
+                .hasRole("USER")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .addFilterBefore(adminSecurityFilter, AnonymousAuthenticationFilter.class)
+                .build();
+    }
 }
